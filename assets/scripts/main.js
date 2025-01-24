94440ecc56ae4db518164fd0654e3bf60d4729a7
Saad Ahmed <saadahmedez1@gmail.com>
2025-01-24 02:37:23 +0200

    Add Header Section And Add Light Mode And Dark Mode

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,9 @@
-
+document.getElementById("dark-mode").onclick = () => {
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.remove("dark");
+    document.getElementById("dark-mode").src = "assets/icons/moon.webp";
+  } else {
+    document.body.classList.add("dark");
+    document.getElementById("dark-mode").src = "assets/icons/sun.webp";
+  }
+};
